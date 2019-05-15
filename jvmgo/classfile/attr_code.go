213a596a780396b3b72395ba91e62f153d2bd51c
zxh0 <zhangxiuhong2013@gmail.com>
2019-05-15 09:33:09 +0800

    remove getters

--- jvmgo/classfile/attr_code.go
+++ jvmgo/classfile/attr_code.go
@@ -37,10 +37,10 @@ func (self *CodeAttribute) readInfo(reader *ClassReader) {
 }
 
 type ExceptionTableEntry struct {
-	startPc   uint16
-	endPc     uint16
-	handlerPc uint16
-	catchType uint16
+	StartPc   uint16
+	EndPc     uint16
+	HandlerPc uint16
+	CatchType uint16
 }
 
 func readExceptionTable(reader *ClassReader) []*ExceptionTableEntry {
@@ -48,24 +48,11 @@ func readExceptionTable(reader *ClassReader) []*ExceptionTableEntry {
 	exceptionTable := make([]*ExceptionTableEntry, exceptionTableLength)
 	for i := range exceptionTable {
 		exceptionTable[i] = &ExceptionTableEntry{
-			startPc:   reader.readUint16(),
-			endPc:     reader.readUint16(),
-			handlerPc: reader.readUint16(),
-			catchType: reader.readUint16(),
+			StartPc:   reader.readUint16(),
+			EndPc:     reader.readUint16(),
+			HandlerPc: reader.readUint16(),
+			CatchType: reader.readUint16(),
 		}
 	}
 	return exceptionTable
-}
-
-func (self *ExceptionTableEntry) StartPc() uint16 {
-	return self.startPc
-}
-func (self *ExceptionTableEntry) EndPc() uint16 {
-	return self.endPc
-}
-func (self *ExceptionTableEntry) HandlerPc() uint16 {
-	return self.handlerPc
-}
-func (self *ExceptionTableEntry) CatchType() uint16 {
-	return self.catchType
 }
